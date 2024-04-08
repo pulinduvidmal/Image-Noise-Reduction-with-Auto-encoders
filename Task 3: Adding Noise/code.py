ca3c53f40f6b56cff368c443adb445c3c17acca1
Pulindu Vidmal <107745680+pulinduvidmal@users.noreply.github.com>
2024-04-08 20:20:18 +0530

    Update code.py

--- Task 3: Adding Noise/code.py
+++ Task 3: Adding Noise/code.py
@@ -1,3 +1,10 @@
+'''
+We add noise to our training and test examples on purpose. But why? Well, in real life, our data often has some unwanted noise, like static on a TV screen. But here's the catch: we usually don't have perfect, noise-free versions of our data to compare against.
+So, to teach our Auto-encoder model how to deal with this noisy data, we first create our own noise and add it to clean images. This way, we have pairs of images: one clean and one noisy. We use these pairs to train the Auto-encoder to focus on the important parts of the images, even when there's noise.
+Then, when we give the Auto-encoder real-world data with noise, it already knows how to handle it. It knows where to look and which details are crucial, even if the data isn't perfect. So, by training with artificially added noise, we're helping the Auto-encoder get better at handling noisy data in the real world.
+'''
+
+
 x_train_noisy = x_train +np.random.rand(60000,784)*1
 x_test_noisy = x_test +np.random.rand(10000,784)*1
 x_train_noisy=np.clip(x_train_noisy,0.,1.)
