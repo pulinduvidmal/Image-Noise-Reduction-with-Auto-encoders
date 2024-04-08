8194ecefb5f32c77d42e46d72d4f2b977c1cfe02
Pulindu Vidmal <107745680+pulinduvidmal@users.noreply.github.com>
2024-04-08 20:39:58 +0530

    Update task_1.py

--- Task 1: Introduction and Importing Libraries/task_1.py
+++ Task 1: Introduction and Importing Libraries/task_1.py
@@ -1,4 +1,3 @@
-'''In this project, we will be working on building two neural network models using the popular MNIST dataset. The MNIST dataset consists of 60,000 examples of handwritten digit images for training and 10,000 examples for testing. Each image is a grayscale 28x28 pixel representation of a handwritten digit from 0 to 9.'''
 
 import numpy as np
 
